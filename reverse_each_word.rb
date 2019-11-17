2f0cc392a356e232d85100bd3b088e5c3dcb4e61
Amanda Rolon <abrolon87@gmail.com>
2019-11-17 02:40:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ def reverse_each_word(sentence)
 end
 
 
-def reverse_each_word(sentence)
-  sentence.reverse.split.reverse.join(" ")
-end
+#def reverse_each_word(sentence)
+#  sentence.reverse.split.reverse.join(" ")
+#end
